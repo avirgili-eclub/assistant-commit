5b8a99dfa181f904ff4f2a2b6c06f09dcb8fef61
avirgili <avirgilitech@gmail.com>
2025-05-09 12:31:58 -0300

    Primer commit del proyecto

---
+++ src/main/java/com/virtech/assistant_commit/AssistantCommitApplication.java
@@ -0,0 +1,13 @@
+package com.virtech.assistant_commit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantCommitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistantCommitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virtech/assistant_commit/AssistantCommitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtech.assistant_commit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantCommitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
